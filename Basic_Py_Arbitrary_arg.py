6a70e11838d97c9a32728b79f9f69095361cbb2d
“Rafael <rafcawicaan@gmail.com>
2025-08-23 18:46:33 +0800

    adding arguments

---
+++ Basic_Py_Arbitrary_arg.py
@@ -0,0 +1,4 @@
+def printFamily(*first_name, last_name):
+    for name in first_name :
+        print(name +" "+ last_name)
+printFamily("John","Bob",last_name="Sample")
---
+++ Basic_Py_Day_Ten(for loop continue).py
@@ -0,0 +1,24 @@
+############################################################################################3
+
+# While Loop
+
+# Ask the user for a lowercase word
+# word = input("Enter a lowercase word: ")
+
+# # Initialize an empty string for reversed word
+# reversed_word = ""
+
+# # Loop through each character in reverse order
+# for i in range(len(word) - 1, -1, -1):
+#     reversed_word += word[i]
+
+# print("Reversed word:", reversed_word)
+
+# # One liner Code for reverse word 
+# print("Reversed word:", input("Enter a lowercase word: ")[::-1])
+
+# #############################################################################################
+# # One liner code for palindrome
+# print("Yes, it is a palindrome." if (w := input("Enter a lowercase word: ")) == w[::-1] else "No, it is not a palindrome.")
+
+
